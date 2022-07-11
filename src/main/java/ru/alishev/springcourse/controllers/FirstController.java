b8e11703d665cf3d1fc44ba857a94fa2c7ee8221
cruelblade <redeptikon@mail.ru>
2022-07-11 13:14:18 +0300

    broken 17 java version

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,18 +13,37 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-            public String helloPage(@RequestParam(value = "name", required = true) String name,
-                                    @RequestParam(value = "surname", required = false) String surname) {
-//    public String helloPage(HttpServletRequest request) {
-//        String name = request.getParameter("name");
-//        String surname = request.getParameter("surname");
+            public String helloPage(@RequestParam(value = "name", required = false) String name,
+                                    @RequestParam(value = "surname", required = false) String surname,
+                                    Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        // System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodBye";
+    }
+
+    @GetMapping("/calculator")
+    public String firstCalculator(@RequestParam(value = "a", required = false) int a,
+                            @RequestParam(value = "b", required = false) int b,
+                            @RequestParam(value = "action", required = false) String action,
+                            Model model) {
+
+        int result = 0;
+        switch (action) {
+            case "multiplication" -> result = a*b;
+            case "addition" -> result = a+b;
+            case "substraction" -> result = a-b;
+            case "division" -> result = (int) a/b;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 }
