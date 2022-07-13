48b3037e643ba1d982129e851b05ca1f4421f4b3
cruelblade <redeptikon@mail.ru>
2022-07-13 13:48:03 +0300

    lesson10 mvc

--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -4,14 +4,15 @@ public class Person {
     private int id;
     private String name;
 
+    public Person() {
+
+    }
+
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public Person() {
-
-    }
 
     public int getId() {
         return id;
